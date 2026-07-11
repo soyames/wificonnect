1ff9b0395796241a6a3bb4d1a55625a3605ca332
Yao Amevi A. Sossou <soyames@gmail.com>
2026-07-11 17:38:47 +0200

    feat: add pdf receipt download for users and operator config

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"3452d735bd38224ef2db85ca763d862d6326b1
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2698259427" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2744132374" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
